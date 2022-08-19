6472e88e7af3866dc19e380927c2471dd7681d36
yunkim <1106qwe@hanmail.net>
2022-08-19 18:56:18 +0900

    - todo list

--- src/main/java/org/example/TodoServerApplication.java
+++ src/main/java/org/example/TodoServerApplication.java
@@ -1,10 +1,11 @@
 package org.example;
 
-import java.util.Arrays;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@SpringBootApplication
 public class TodoServerApplication {
     public static void main(String[] args) {
-        System.out.println("Hello");
-
+        SpringApplication.run(TodoServerApplication.class, args);
     }
 }
--- src/main/java/org/example/controller/TodoController.java
+++ src/main/java/org/example/controller/TodoController.java
@@ -1,10 +1,12 @@
 package org.example.controller;
 
 import lombok.AllArgsConstructor;
+import org.example.model.TodoResponse;
 import org.example.service.TodoService;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @CrossOrigin//CROS 해결하는 방법
 @AllArgsConstructor
@@ -15,4 +17,51 @@ RestController의 주 용도는 Json 형태로 객체 데이터를 반환
 @RequestMapping("/")//우리가 특정 URL을 보내면 어떠헥 처리할 것인지 정의한다.
 public class TodoController {
     private final TodoService service;
+
+    //POST 아이템 추가
+    @PostMapping
+    public ResponseEntity<TodoResponse> create(){
+        System.out.println("Create");
+
+        return null;
+    }
+
+    //GET 전체 조회
+    @GetMapping("{id}")
+    public ResponseEntity<TodoResponse> readOne(){
+        System.out.println("Read One");
+
+        return null;
+    }
+
+    //GET 부분 조회
+    @GetMapping
+    public ResponseEntity<List<TodoResponse>> readAll(){
+        System.out.println("Read All");
+
+        return null;
+    }
+
+    //PATH 수정
+    @PatchMapping("{id}")
+    public ResponseEntity<TodoResponse> update(){
+        System.out.println("Update");
+
+        return null;
+    }
+
+    //DELETE 부분 삭제
+    @DeleteMapping("{id}")
+    public ResponseEntity<?> deleteOne(){
+        System.out.println("Deltet One");
+
+        return null;
+    }
+
+    //DELETE 전체 삭제
+    public ResponseEntity<?> deleteAll(){
+        System.out.println("Delete All");
+
+        return null;
+    }
 }
